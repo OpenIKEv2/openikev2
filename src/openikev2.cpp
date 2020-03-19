985b7678bc615f9b1247b7889ec0bcde670e8fbe
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2020-03-19 09:35:12 +0100

    Change license in file headers as well

--- src/openikev2.cpp
+++ src/openikev2.cpp
@@ -3,20 +3,8 @@
 *   Pedro J. Fernandez Ruiz    pedroj@um.es                               *
 *   Alejandro Perez Mendez     alex@um.es                                 *
 *                                                                         *
-*   This library is free software; you can redistribute it and/or         *
-*   modify it under the terms of the GNU Lesser General Public            *
-*   License as published by the Free Software Foundation; either          *
-*   version 2.1 of the License, or (at your option) any later version.    *
-*                                                                         *
-*   This library is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU     *
-*   Lesser General Public License for more details.                       *
-*                                                                         *
-*   You should have received a copy of the GNU Lesser General Public      *
-*   License along with this library; if not, write to the Free Software   *
-*   Foundation, Inc., 51 Franklin St, Fifth Floor,                        *
-*   Boston, MA  02110-1301  USA                                           *
+*   This software may be modified and distributed under the terms         *
+*   of the Apache license.  See the LICENSE file for details.             *
 ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
